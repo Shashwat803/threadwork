2436f4e54803ed3424d6befc4c0eed42c973ecd5
Shashwat803 <shashwatmishra803@gmail.com>
2024-07-29 01:01:45 +0530

    updated API error class and added asyncHandler Method

--- backend/src/utils/ApiError.ts
+++ backend/src/utils/ApiError.ts
@@ -1,20 +1,29 @@
 class ApiError extends Error {
-    statusCode: Number
-    data: null
-    success: boolean
-    errors: any
-    constructor(statusCode: Number, message = "Something Went wrong", errors = [], stack = "") {
-        super(message)
-        this.statusCode = statusCode
-        this.data = null
-        this.message = message
-        this.success = false
-        this.errors = errors
+    statusCode: number;
+    data: null;
+    success: boolean;
+    errors: any[];
+    message: string;
+
+    constructor(
+        statusCode: number,
+        message: string = "Something went wrong",
+        errors: any[] = [],
+        stack: string = ""
+    ) {
+        super(message);
+        this.statusCode = statusCode;
+        this.data = null;
+        this.message = message;
+        this.success = false;
+        this.errors = errors;
 
         if (stack) {
-            this.stack = stack
+            this.stack = stack;
         } else {
-            Error.captureStackTrace(this, this.constructor)
+            Error.captureStackTrace(this, this.constructor);
         }
     }
 }
+
+export default ApiError;
